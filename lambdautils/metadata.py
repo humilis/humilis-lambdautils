71f501d1b1f306b9b8f566effcfd75ef70d7c448
German Gomez-Herrero <german@innovativetravel.eu>
2016-03-09 22:53:52 +0100

    Release 0.0.4: dealing with DynamoDB or KMS errors

--- lambdautils/metadata.py
+++ lambdautils/metadata.py
@@ -3,7 +3,7 @@
 
 package = "lambdautils"
 project = "lambdautils"
-version = '0.0.3'
+version = '0.0.4'
 description = "Simple utilities for AWS Lambda functions"
 authors = ["Innovative Travel Ltd"]
 authors_string = ', '.join(authors)
