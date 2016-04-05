88d938fc4050ef99180ff364f0a6d27d31ecc16c
German Gomez-Herrero <german@innovativetravel.eu>
2016-04-05 16:27:57 +0200

    Support for externally produced Kinesis partition keys

--- lambdautils/metadata.py
+++ lambdautils/metadata.py
@@ -3,7 +3,7 @@
 
 package = "lambdautils"
 project = "lambdautils"
-version = '0.2.8'
+version = '0.2.9'
 description = "Simple utilities for AWS Lambda functions"
 authors = ["Innovative Travel Ltd"]
 authors_string = ', '.join(authors)
