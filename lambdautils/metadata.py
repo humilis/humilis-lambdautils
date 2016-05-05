54c3136b501750fd043ca4406690e49c23399a88
German Gomez-Herrero <german@innovativetravel.eu>
2016-05-05 19:53:58 +0200

    Release 0.4.7

--- lambdautils/metadata.py
+++ lambdautils/metadata.py
@@ -3,7 +3,7 @@
 
 package = "lambdautils"
 project = "lambdautils"
-version = '0.4.6'
+version = '0.4.7'
 description = "Simple utilities for AWS Lambda functions"
 authors = ["Innovative Travel Ltd"]
 authors_string = ', '.join(authors)
