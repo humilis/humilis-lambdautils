16b3dcd203d4d79b7e210f9d65195bc9c5e74420
German Gomez-Herrero <german@innovativetravel.eu>
2016-09-15 16:59:27 +0200

    Release 0.7.2

--- lambdautils/__init__.py
+++ lambdautils/__init__.py
@@ -1,5 +1,5 @@
 """Utilities for AWS Lambda functions."""
 
 
-__version__ = "0.7.1"
-__author__ = "German Gomez-Herrero, FindHotel BV"
+__version__ = "0.7.2"
+__author__ = "German Gomez-Herrero, Arnaud Charpentier, FindHotel BV"
