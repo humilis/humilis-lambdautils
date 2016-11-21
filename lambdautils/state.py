022cca788e4ed601e487e365eebb873d32272f66
German Gomez-Herrero <german@innovativetravel.eu>
2016-11-21 16:13:49 +0100

    Fix log message

--- lambdautils/state.py
+++ lambdautils/state.py
@@ -291,7 +291,7 @@ def produce_context(namespace, context_id, max_delay=None):
     """Produce event context."""
     try:
         context_obj = get_context(namespace, context_id)
-        logger.info("Found context '%s:%s'".format(namespace, context_id))
+        logger.info("Found context '%s:%s'", namespace, context_id)
     except ContextError:
         logger.info("Context '%s:%s' not found", namespace, context_id)
         if max_delay is not None:
