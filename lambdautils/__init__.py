f5d361bf76f0a6fa410acecc78381498b5df6e94
German Gomez-Herrero <german@innovativetravel.eu>
2017-05-29 11:51:11 +0200

    Delivering and retrieving events from Kinesis is more flexible

--- lambdautils/__init__.py
+++ lambdautils/__init__.py
@@ -1,5 +1,5 @@
 """Utilities for AWS Lambda functions."""
 
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __author__ = "German Gomez-Herrero, Arnaud Charpentier, FindHotel BV"
--- lambdautils/kinesis.py
+++ lambdautils/kinesis.py
@@ -20,7 +20,7 @@ class BadKinesisEventError(Exception):
     pass
 
 
-def unpack_kinesis_event(kinesis_event, deserializer=None, preprocess=None,
+def unpack_kinesis_event(kinesis_event, deserializer=None, unpacker=None,
                          embed_timestamp=False):
     """Extracts events (a list of dicts) from a Kinesis event."""
     records = kinesis_event["Records"]
@@ -28,8 +28,8 @@ def unpack_kinesis_event(kinesis_event, deserializer=None, preprocess=None,
     shard_ids = set()
     for rec in records:
         payload = base64.decodestring(rec["kinesis"]["data"])
-        if preprocess:
-            payload = preprocess(payload)
+        if unpacker:
+            payload = unpacker(payload)
         payload = payload.decode()
         shard_ids.add(rec["eventID"].split(":")[0])
         if deserializer:
@@ -88,7 +88,7 @@ def send_to_delivery_stream(events, stream_name):
 
 
 def send_to_kinesis_stream(events, stream_name, partition_key=None,
-                           preprocess=None, serializer=json.dumps):
+                           packer=None, serializer=json.dumps):
     """Sends events to a Kinesis stream."""
     if not events:
         logger.info("No events provided: nothing delivered to Firehose")
@@ -106,8 +106,8 @@ def send_to_kinesis_stream(events, stream_name, partition_key=None,
         if not isinstance(event, str):
             event = serializer(event)
 
-        if preprocess:
-            event = preprocess(event)
+        if packer:
+            event = packer(event)
 
         record = {"Data": event,
                   "PartitionKey": partition_key_value}
